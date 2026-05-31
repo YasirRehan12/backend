0cfffa27639eef5678db3209854cfcdb69ca4d5b
YasirRehan12 <yasirgithub12@gmail.com>
2026-05-31 15:59:06 +0500

    Initial backend upload

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const mongoose = require("mongoose");
 const app = express();
 
 const corsOptions = {
-    origin: "https://frontend-nine-theta-59.vercel.app",
+    origin: process.env.FRONTEND_URL,
     methods: ["GET", "POST", "DELETE", "PUT", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
